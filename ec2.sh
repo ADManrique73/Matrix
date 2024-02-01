4d0719563e81c48ec2a31b05661a7aa023a410c8
ADManrique73 <157367016+ADManrique73@users.noreply.github.com>
2024-02-01 19:53:48 +0100

    ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -2,34 +2,76 @@
 
 #AsierDM 
 
-# Variables
-AMI_ID="ami-0c7217cdde317cfec"  
-INSTANCE_TYPE="t2.micro"
-KEY_NAME="asier.pem"
-SECURITY_GROUP_ID="sg-0cac8380b6a72f987"  
-SUBNET_ID="subnet-05e92731a4c7f6ccf"  
-USER_DATA_SCRIPT="script.sh"  
  
-# Crear la instancia EC2
-INSTANCE_ID=$(aws ec2 run-instances \
-  --image-id $AMI_ID \
-  --instance-type $INSTANCE_TYPE \
-  --key-name $KEY_NAME \
-  --security-group-ids $SECURITY_GROUP_ID \
-  --subnet-id $SUBNET_ID \
-  --user-data file://$USER_DATA_SCRIPT \
-  --query 'Instances[0].InstanceId' \
-  --output text)
+# Definir variables
+NOMBRE_VPC="vpc-AsierDM"
+CIDR_VPC="10.207.0.0/16"
+NOMBRE_SUBRED_PUBLICA="SubredPublica-Asier"
+CIDR_SUBRED_PUBLICA="10.207.1.0/24"
+NOMBRE_SUBRED_PRIVADA="SubredPrivada-Asier"
+CIDR_SUBRED_PRIVADA="10.207.2.0/24"
+UBUNTU="ami-0c7217cdde317cfec"
+KEY_NAME="asier"
  
-# Esperar hasta que la instancia esté en ejecución
-echo "Esperando a que la instancia esté en ejecución..."
-aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+# Crear VPC
+vpcId=$(aws ec2 create-vpc --cidr-block $CIDR_VPC --output json | jq -r '.Vpc.VpcId')
+aws ec2 create-tags --resources $vpcId --tags Key=Name,Value=$NOMBRE_VPC
  
-# Obtener la dirección IP pública de la instancia
-PUBLIC_IP=$(aws ec2 describe-instances \
-  --instance-ids $INSTANCE_ID \
-  --query 'Reservations[0].Instances[0].PublicIpAddress' \
-  --output text)
+# Crear subred pública
+subnetPublicaId=$(aws ec2 create-subnet --vpc-id $vpcId --cidr-block $CIDR_SUBRED_PUBLICA --availability-zone us-east-1a --output json | jq -r '.Subnet.SubnetId')
+aws ec2 create-tags --resources $subnetPublicaId --tags Key=Name,Value=$NOMBRE_SUBRED_PUBLICA
  
-echo "La instancia con ID $INSTANCE_ID está en ejecución."
-echo "Dirección IP pública: $PUBLIC_IP"
+# Crear subred privada
+subnetPrivadaId=$(aws ec2 create-subnet --vpc-id $vpcId --cidr-block $CIDR_SUBRED_PRIVADA --availability-zone us-east-1a --output json | jq -r '.Subnet.SubnetId')
+aws ec2 create-tags --resources $subnetPrivadaId --tags Key=Name,Value=$NOMBRE_SUBRED_PRIVADA
+ 
+# Crear Internet Gateway
+internetGatewayId=$(aws ec2 create-internet-gateway --output json | jq -r '.InternetGateway.InternetGatewayId')
+aws ec2 create-tags --resources $internetGatewayId --tags Key=Name,Value=${NOMBRE_VPC}-IG
+ 
+# Adjuntar Internet Gateway a la VPC
+aws ec2 attach-internet-gateway --vpc-id $vpcId --internet-gateway-id $internetGatewayId
+ 
+# Crear tabla de rutas pública
+routeTablePublicId=$(aws ec2 create-route-table --vpc-id $vpcId --output json | jq -r '.RouteTable.RouteTableId')
+ 
+# Crear ruta para tráfico público
+aws ec2 create-route --route-table-id $routeTablePublicId --destination-cidr-block 0.0.0.0/0 --gateway-id $internetGatewayId
+ 
+# Asociar tabla de rutas pública a la subred pública
+aws ec2 associate-route-table --subnet-id $subnetPublicaId --route-table-id $routeTablePublicId
+ 
+#Crear Gateway NAT
+allocationId=$(aws ec2 allocate-address --domain vpc --output json | jq -r '.AllocationId')
+natGatewayId=$(aws ec2 create-nat-gateway --subnet-id $subnetPublicaId --allocation-id $allocationId --output json | jq -r '.NatGateway.NatGatewayId')
+ 
+# Esperar hasta que el Nat Gateway esté disponible
+echo "Esperando a que el Nat Gateway esté disponible..."
+aws ec2 wait nat-gateway-available --nat-gateway-ids $natGatewayId
+ 
+# Crear tabla de rutas privada
+routeTablePrivadaId=$(aws ec2 create-route-table --vpc-id $vpcId --output json | jq -r '.RouteTable.RouteTableId')
+ 
+# Crear ruta para tráfico privado a través del Gateway NAT
+aws ec2 create-route --route-table-id $routeTablePrivadaId --destination-cidr-block 0.0.0.0/0 --nat-gateway-id $natGatewayId
+ 
+# Asociar tabla de rutas privada a la subred privada
+aws ec2 associate-route-table --subnet-id $subnetPrivadaId --route-table-id $routeTablePrivadaId
+ 
+#Lanzar instancias (Ubuntu t2.micro) en la subred publica
+aws ec2 run-instances \
+    --image-id $UBUNTU \
+    --count 1 \
+    --instance-type t2.micro \
+    --key-name $KEY_NAME \
+    --subnet-id $subnetPublicaId \
+    --associate-public-ip-address
+ 
+#Lanzar instancias (Ubuntu t2.micro) en la subred privada
+ 
+aws ec2 run-instances \
+    --image-id $UBUNTU \
+    --count 1 \
+    --instance-type t2.micro \
+    --key-name $KEY_NAME \
+    --subnet-id $subnetPrivadaId \
