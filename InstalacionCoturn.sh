3197e54116134d3cff4667e891404c18c6cc5040
ADManrique73 <157367016+ADManrique73@users.noreply.github.com>
2024-02-02 10:30:08 +0100

    InstalacionCoturn.sh

--- InstalacionCoturn.sh
+++ InstalacionCoturn.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-#AsierDM 
+#Autor: AsierDM 
 
+#Actualizar máquina
 sudo apt update
 sudo apt upgrade -y
+
+#Instalar coturn
 sudo apt install coturn -y
 sudo systemctl status coturn
