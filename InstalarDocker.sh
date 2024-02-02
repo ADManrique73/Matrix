066fb166c9ee558d0f2d775c61177575890c6ec6
ADManrique73 <157367016+ADManrique73@users.noreply.github.com>
2024-02-02 10:27:24 +0100

    InstalarDocker.sh

--- InstalarDocker.sh
+++ InstalarDocker.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-#AsierDM 
+#Autor: AsierDM 
 
+#Actualizar máquina
 sudo apt update
 sudo apt upgrade -y
+
+#Instalar y actualizar repositorios
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
+
+#instalar docker
 sudo apt install docker-ce -y
 
+#comprobar estado de docker
 sudo systemctl status docker
